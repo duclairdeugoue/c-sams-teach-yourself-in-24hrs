45fb9f86be9c36449b362e31b2818836134cd43d
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-11-03 15:36:11 +0100

    Update structure

---
+++ 16-hour/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - functions that executes certains programs base on user choice
+ * Return 0 if everything is okay
+*/
+int main(void)
+{
+	return (0);
+}
